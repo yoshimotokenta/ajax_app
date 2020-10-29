2a939bdc23667c92a9528ccbacd129588ca10838
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-10-29 11:47:40 +0900

    ajax-Appの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all.order(id: "DESC")
   end
+ 
+   # 削除
+ 
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+ 
  end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_110625) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "cheked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
